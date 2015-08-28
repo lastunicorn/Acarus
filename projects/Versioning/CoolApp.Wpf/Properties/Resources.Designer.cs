c83203f33a6751752db907b0a231f4465bab155b
lastunicorn <coldblackstar@yahoo.com>
2015-08-28 10:26:37 +0300

    added copyright information in cs files

--- projects/Versioning/CoolApp.Wpf/Properties/Resources.Designer.cs
+++ projects/Versioning/CoolApp.Wpf/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfApplicationExit.Properties
+namespace CoolApp.Wpf.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace WpfApplicationExit.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfApplicationExit.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CoolApp.Wpf.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
